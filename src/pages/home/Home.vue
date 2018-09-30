94274bc3e79f55d962163607a5ce51d35c036cf3
tiandandan <dan12378949@163.com>
2018-09-30 15:19:01 +0800

    layout and tree component

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,53 @@
+<template>
+	<div id="root">
+		<div class="layout_head"></div>
+		<div class="layout_main">
+		  <div class="layout_left">
+			  <menu-tree></menu-tree>
+		  </div>
+		  <div class="layout_right"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+import menuTree from './components/menuTree'
+export default {
+  name: 'Home',
+  components: {
+    menuTree
+  }
+}
+</script>
+
+<style scoped="scoped">
+/*.border-bottom{*/
+	/*&:before*/
+	/*border-color: #ccc*/
+/*}*/
+.layout_head{
+	height: 29px;
+	overflow: hidden;
+	background-color: #dcedff;
+	border-bottom: 1px solid #87b9e8;
+}
+.layout_main{
+	position: absolute;
+	top: 30px;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #0000f6;
+}
+.layout_left{
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	width: 199px;
+	overflow: scroll;
+	border-right: 1px solid #87b9e8;
+	background-color: #fff;
+
+}
+</style>
---
+++ src/pages/home/components/menuTree.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>menuTree</div>
+</template>
+
+<script>
+	export default {
+		name: 'menuTree'
+	}
+</script>
+
+<style scoped="scoped">
+
+
+</style>
+
